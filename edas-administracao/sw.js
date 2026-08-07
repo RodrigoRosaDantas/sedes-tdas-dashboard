9df794d765723ef1daf44fe788a26e19eccf3f50
RodrigoRosaDantas <rodrigo.rosa2@outlook.com>
2026-08-07 20:52:08 -0300

    Versionar PWA EDAS 20260807.7

--- edas-administracao/sw.js
+++ edas-administracao/sw.js
@@ -1,4 +1,4 @@
-const VERSION='edas-20260807.6';
+const VERSION='edas-20260807.7';
 const BASE='/sedes-tdas-dashboard/edas-administracao/';
 const USER_CACHE_PREFIX='edas-sec-user-';
 const MATERIAL_USER_CACHE_PREFIX='edas-material-user-';
