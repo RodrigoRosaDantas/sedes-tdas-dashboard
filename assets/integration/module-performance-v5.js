4ac8a239434354db953ae3ffe3b68b96cefdac5a
RodrigoRosaDantas <rodrigo.rosa2@outlook.com>
2026-08-13 18:25:42 -0300

    Usar runtime Firebase no desempenho

--- assets/integration/module-performance-v5.js
+++ assets/integration/module-performance-v5.js
@@ -1,3 +1,3 @@
-import './private-history-runtime.js?v=1.0.0';
+import './private-history-runtime-v2.js?v=2.0.0';
 import './module-performance-v4.js?v=4.0.0';
 import './performance-history-links.js?v=1.0.0';
