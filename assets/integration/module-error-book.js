204e86805e18ec402ec388d741a12bcee6bd0524
RodrigoRosaDantas <rodrigo.rosa2@outlook.com>
2026-08-13 19:29:22 -0300

    Ativar Caderno sincronizado

--- assets/integration/module-error-book.js
+++ assets/integration/module-error-book.js
@@ -1,3 +1,1 @@
-import './private-history-runtime.js?v=1.0.0';
-import './module-error-book-base.js?v=2.0.0';
-import './error-book-enhancer.js?v=1.0.0';
+import './module-error-book-v3.js?v=3.0.0';
